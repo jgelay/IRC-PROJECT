2555c75d505fb5661eba81016ba80a15b07fb070
Josiah Gelay <josiahgelay@gmail.com>
2018-06-04 23:29:26 -0600

    Second real commit

--- ClientApplication/ClientApplication/ClientApplication/MainLauncher.cs
+++ ClientApplication/ClientApplication/ClientApplication/MainLauncher.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientApplication
 {
-    class Main
+    class MainLauncher
     {
+        static void Main(string[] args)
+        {
+            ClientApplication.Client client = new ClientApplication.Client();
+
+            using (NetworkStream stream = client.getClient().GetStream())
+            {
+                client.passwordRegistration(stream);
+            }
+        }
     }
 }
